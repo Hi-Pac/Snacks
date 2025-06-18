0ff6de07b96907a73a2d83ae214ee719a7d336aa
Hi-Pac <mo.tiger.789@gmail.com>
2025-06-19 00:13:03 +0300

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,13 +1,13 @@
 // Firebase Configuration
 // يجب استبدال هذه القيم بقيم مشروعك الفعلي من Firebase Console
 const firebaseConfig = {
-  apiKey: "AIzaSyCAUaKXd9bzMUfBAQTa1nSaEbR_VVLIe98",
-  authDomain: "colorflow-erp.firebaseapp.com",
-  projectId: "colorflow-erp",
-  storageBucket: "colorflow-erp.firebasestorage.app",
-  messagingSenderId: "40753390221",
-  appId: "1:40753390221:web:a032845d5891d2b510b8c4",
-  measurementId: "G-RBL9VL7RC0"
+    apiKey: "AIzaSyCAUaKXd9bzMUfBAQTa1nSaEbR_VVLIe98",
+    authDomain: "colorflow-erp.firebaseapp.com",
+    projectId: "colorflow-erp",
+    storageBucket: "colorflow-erp.firebasestorage.app",
+    messagingSenderId: "40753390221",
+    appId: "1:40753390221:web:a032845d5891d2b510b8c4",
+    measurementId: "G-RBL9VL7RC0"
 };
 
 // Initialize Firebase
